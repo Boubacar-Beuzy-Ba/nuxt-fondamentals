3094134bffa46b7471dcf69b801c0a7e0dfc68d7
Boubacar-Beuzy-Ba <bouba0178@gmail.com>
2020-06-30 21:38:29 +0000

    make it better

---
+++ store/posts.js
@@ -0,0 +1,20 @@
+export const state = () => ({
+  all: [
+    {
+      id:'nuxt',
+      title: 'NuxtJS',
+      content: 'Build your next Vue.js application with confidence using NuxtJS. An open source framework making web development simple and powerful',
+    },
+    {
+      id: 'angular',
+      title: 'Angular',
+      content: 'Angular is an application design framework and development platform for creating efficient and sophisticated single-page apps.',
+    },
+    {
+      id: 'react',
+      title: 'React',
+      content: 'React is a JavaScript library for building user interfaces'
+    }
+
+  ]
+})
